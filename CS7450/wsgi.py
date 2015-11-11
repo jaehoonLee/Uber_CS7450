e9f2a756279c9f3a73f18c7cab18993108968293
JaeHoon Lee <jaehoonlee88@gmail.com>
2015-11-11 15:14:10 -0500

    init

---
+++ CS7450/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for CS7450 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CS7450.settings")
+
+application = get_wsgi_application()
